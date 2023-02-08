7dee6c9c4e87878416708ec7265689f3bbf82ae8
minji.ji <mwinterj@daum.net>
2023-02-08 22:00:46 +0900

    :sparkles: Feat: 아이스크림 생성하는 로직 구현

---
+++ assets/script/components/iceCreamGenerator.js
@@ -0,0 +1,42 @@
+class IceCreamGenerator {
+  // constructor는 인스턴스 생성 및 초기화하는 메서드
+  constructor() {
+    this.itemList = document.querySelector(".list-item");
+  }
+
+  async setup() {
+    await this.loadData((json) => {
+      this.iceFactory(json);
+    });
+  }
+
+  async loadData(callback) {
+    const response = await fetch("assets/script/item.json");
+
+    if (response.ok) {
+      callback(await response.json());
+    } else {
+      alert("통신 에러!" + response.status);
+    }
+  }
+
+  // 아이스크림을 생산하는 함수입니다.
+  iceFactory(data) {
+    const docFrag = document.createDocumentFragment();
+    data.forEach((el) => {
+      const item = document.createElement("li");
+      const itemTemplate = `
+            <button type="button" class="btn-item" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
+                <img src="./assets/images/${el.img}" alt="" class="img-item">
+                <strong class="tit-item">${el.name}</strong>
+                <span class="txt-price">${el.cost}</span>
+            </button>
+            `;
+      item.innerHTML = itemTemplate;
+      docFrag.appendChild(item);
+    });
+    this.itemList.appendChild(docFrag);
+  }
+}
+
+export default IceCreamGenerator;
