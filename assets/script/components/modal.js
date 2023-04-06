5a9b5b1d04a805b0499cbe857b39524ed58deb5b
minji.jo <mwinterj@daum.net>
2023-04-07 00:36:23 +0900

    :sparkles: Feat: 클릭한 버튼의 상황에 맞는 Modal을 생성하는 createModal 로직 구현

--- assets/script/components/modal.js
+++ assets/script/components/modal.js
@@ -1,11 +1,34 @@
 class Modal {
   constructor() {}
 
-  createModal() {
+  createModal(text, textBold) {
+    console.log(text, textBold);
     const $body = document.querySelector("body");
-    console.log($body);
-    const modalWrap = document.createElement("div");
-    modalWrap.classList.add("modal");
+
+    const modalWrap = document.createElement("article");
+    modalWrap.classList.add("modal-wrap");
+
+    const irText = document.createElement("h2");
+    irText.classList.add("ir");
+    irText.textContent = "알림창";
+
+    const closeModalBtn = document.createElement("button");
+    closeModalBtn.setAttribute("type", "button");
+    closeModalBtn.classList.add("btn-close-modal");
+
+    const modalDiv = document.createElement("div");
+    modalDiv.classList.add("modal");
+
+    const modalInfoText = document.createElement("p");
+    modalInfoText.classList.add("modal-info-text");
+    modalInfoText.textContent = text;
+
+    const modalInfoTextBold = document.createElement("strong");
+    modalInfoTextBold.classList.add("modal-info-text-bold");
+    modalInfoTextBold.textContent = textBold;
+
+    modalDiv.append(modalInfoText, modalInfoTextBold);
+    modalWrap.append(irText, closeModalBtn, modalDiv);
     $body.appendChild(modalWrap);
   }
 }
